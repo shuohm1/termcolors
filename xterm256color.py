9581a5d8b694ba44273cac82e95436b3f718158a
Shu Ohmichi <shu.ohm1@gmail.com>
2017-09-02 09:22:24 +0900

    clean: move processes in the main function

--- xterm256color.py
+++ xterm256color.py
@@ -3,6 +3,8 @@
 
 from __future__ import print_function
 
+import sys
+
 DEFAULTS = 16
 DEF_BREAKS = 8
 
@@ -29,7 +31,8 @@ def gray_range(grads):
 	for i in range(grads):
 		yield (i, i * GRAY_STEP + GRAY_BASE)
 
-print("""
+def main():
+	print("""
 ***********************************************************************
 
 This script shows default 256 colors of 'xterm-256colors'. If you
@@ -41,38 +44,43 @@ xx#rrggbb
 \\_ an indexed color code
 
 ***********************************************************************
-""".strip())
+	""".strip())
 
-s = "{e}[38;5;{{code:d}}m{{code:02x}}#{{hexs}}{e}[m"
-t = s.format(e="\033")
+	s = "{e}[38;5;{{code:d}}m{{code:02x}}#{{hexs}}{e}[m"
+	t = s.format(e="\033")
 
-h = "******"
-for c in range(DEFAULTS):
-	print(t.format(code=c, hexs=h), end="")
-	if (c + 1) % DEF_BREAKS > 0:
-		print(" ", end="")
-	else:
-		print()
-base = c + 1
+	h = "******"
+	for c in range(DEFAULTS):
+		print(t.format(code=c, hexs=h), end="")
+		if (c + 1) % DEF_BREAKS > 0:
+			print(" ", end="")
+		else:
+			print()
+	base = c + 1
 
-for i, r in rgb_range(RGB_GRADS):
-	for j, g in rgb_range(RGB_GRADS):
-		for k, b in rgb_range(RGB_GRADS):
-			c = base + i * RGB_GRADS ** 2 + j * RGB_GRADS + k
-			h = "{r:02x}{g:02x}{b:02x}".format(r=r, g=g, b=b)
-			print(t.format(code=c, hexs=h), end="")
-			if (c - base + 1) % RGB_BREAKS > 0:
-				print(" ", end="")
-			else:
-				print()
-base = c + 1
+	for i, r in rgb_range(RGB_GRADS):
+		for j, g in rgb_range(RGB_GRADS):
+			for k, b in rgb_range(RGB_GRADS):
+				c = base + i * RGB_GRADS ** 2 + j * RGB_GRADS + k
+				h = "{r:02x}{g:02x}{b:02x}".format(r=r, g=g, b=b)
+				print(t.format(code=c, hexs=h), end="")
+				if (c - base + 1) % RGB_BREAKS > 0:
+					print(" ", end="")
+				else:
+					print()
+	base = c + 1
 
-for i, k in gray_range(GRAY_GRADS):
-	c = base + i
-	h = "{k:02x}{k:02x}{k:02x}".format(k=k)
-	print(t.format(code=c, hexs=h), end="")
-	if (c - base + 1) % GRAY_BREAKS > 0:
-		print(" ", end="")
-	else:
-		print()
-base = c + 1
+	for i, k in gray_range(GRAY_GRADS):
+		c = base + i
+		h = "{k:02x}{k:02x}{k:02x}".format(k=k)
+		print(t.format(code=c, hexs=h), end="")
+		if (c - base + 1) % GRAY_BREAKS > 0:
+			print(" ", end="")
+		else:
+			print()
+	base = c + 1
+
+	return 0
+
+if __name__ == "__main__":
+	sys.exit(main())
