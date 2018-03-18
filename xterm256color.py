fff4e412ddd80f387f8cf622020a2b83bd01857f
Shu Ohmichi <shu.ohm1@gmail.com>
2018-03-18 19:14:57 +0900

    change: replace tabs with whitespaces

--- xterm256color.py
+++ xterm256color.py
@@ -31,70 +31,70 @@ indexed color codes and RGB color codes are not correspond.
 """
 
 def rgb_range(grads):
-	yield (0, 0)
-	for i in range(1, grads):
-		yield (i, i * RGB_STEP + RGB_BIAS)
+  yield (0, 0)
+  for i in range(1, grads):
+    yield (i, i * RGB_STEP + RGB_BIAS)
 
 def gray_range(grads):
-	for i in range(grads):
-		yield (i, i * GRAY_STEP + GRAY_BASE)
+  for i in range(grads):
+    yield (i, i * GRAY_STEP + GRAY_BASE)
 
 def print_codes(codes, breaks):
-	print_codelines(codes[i:i + breaks]
-	                for i in range(0, len(codes), breaks))
+  print_codelines(codes[i:i + breaks]
+                  for i in range(0, len(codes), breaks))
 
 def print_codelines(lines):
-	for codes in lines:
-		print(" ".join(codes))
+  for codes in lines:
+    print(" ".join(codes))
 
 def main(show_basic=False, show_colorful=False, show_gray=False):
-	show_all = not (show_basic or show_colorful or show_gray)
+  show_all = not (show_basic or show_colorful or show_gray)
 
-	s = "{e}[38;5;{{code:d}}m{{code:02x}}#{{hexs}}{e}[m"
-	t = s.format(e="\033")
-	base = 0
+  s = "{e}[38;5;{{code:d}}m{{code:02x}}#{{hexs}}{e}[m"
+  t = s.format(e="\033")
+  base = 0
 
-	if show_basic or show_all:
-		h = "******"
-		basic_codes = [t.format(code=c, hexs=h) for c in range(DEFAULTS)]
-		print_codes(basic_codes, DEF_BREAKS)
-	base += DEFAULTS
+  if show_basic or show_all:
+    h = "******"
+    basic_codes = [t.format(code=c, hexs=h) for c in range(DEFAULTS)]
+    print_codes(basic_codes, DEF_BREAKS)
+  base += DEFAULTS
 
-	if show_colorful or show_all:
-		colorful_codes = []
-		for i, r in rgb_range(RGB_GRADS):
-			for j, g in rgb_range(RGB_GRADS):
-				for k, b in rgb_range(RGB_GRADS):
-					c = base + i * RGB_GRADS ** 2 + j * RGB_GRADS + k
-					h = "{r:02x}{g:02x}{b:02x}".format(r=r, g=g, b=b)
-					colorful_codes.append(t.format(code=c, hexs=h))
-		print_codes(colorful_codes, RGB_BREAKS)
-	base += RGB_GRADS ** 3
+  if show_colorful or show_all:
+    colorful_codes = []
+    for i, r in rgb_range(RGB_GRADS):
+      for j, g in rgb_range(RGB_GRADS):
+        for k, b in rgb_range(RGB_GRADS):
+          c = base + i * RGB_GRADS ** 2 + j * RGB_GRADS + k
+          h = "{r:02x}{g:02x}{b:02x}".format(r=r, g=g, b=b)
+          colorful_codes.append(t.format(code=c, hexs=h))
+    print_codes(colorful_codes, RGB_BREAKS)
+  base += RGB_GRADS ** 3
 
-	if show_gray or show_all:
-		gray_codes = []
-		for i, k in gray_range(GRAY_GRADS):
-			c = base + i
-			h = "{k:02x}{k:02x}{k:02x}".format(k=k)
-			gray_codes.append(t.format(code=c, hexs=h))
-		print_codes(gray_codes, GRAY_BREAKS)
-	base += GRAY_GRADS
+  if show_gray or show_all:
+    gray_codes = []
+    for i, k in gray_range(GRAY_GRADS):
+      c = base + i
+      h = "{k:02x}{k:02x}{k:02x}".format(k=k)
+      gray_codes.append(t.format(code=c, hexs=h))
+    print_codes(gray_codes, GRAY_BREAKS)
+  base += GRAY_GRADS
 
-	return 0
+  return 0
 
 def parse(argv):
-	parser = argparse.ArgumentParser(description=DESCRIPTION,
-	                                 epilog=EPILOG)
-	parser.add_argument("-b", "--basic", dest="show_basic",
-	                    action="store_true", default=False,
-	                    help="show basic color codes")
-	parser.add_argument("-c", "--colorful", dest="show_colorful",
-	                    action="store_true", default=False,
-	                    help="show RGB color codes")
-	parser.add_argument("-g", "--gray", dest="show_gray",
-	                    action="store_true", default=False,
-	                    help="show gray color codes")
-	return parser.parse_args(argv)
+  parser = argparse.ArgumentParser(description=DESCRIPTION,
+                                   epilog=EPILOG)
+  parser.add_argument("-b", "--basic", dest="show_basic",
+                      action="store_true", default=False,
+                      help="show basic color codes")
+  parser.add_argument("-c", "--colorful", dest="show_colorful",
+                      action="store_true", default=False,
+                      help="show RGB color codes")
+  parser.add_argument("-g", "--gray", dest="show_gray",
+                      action="store_true", default=False,
+                      help="show gray color codes")
+  return parser.parse_args(argv)
 
 if __name__ == "__main__":
-	sys.exit(main(**vars(parse(sys.argv[1:]))))
+  sys.exit(main(**vars(parse(sys.argv[1:]))))
